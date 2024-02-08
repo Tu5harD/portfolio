ccd06eea2dc7ee23dcf3a2acf52ea35a0895bd31
DevTusharDukane <tushardukane9@gmail.com>
2024-02-08 16:48:34 +0530

    Final Change

--- src/container/Header/index.js
+++ src/container/Header/index.js
@@ -58,7 +58,6 @@ const Header = () => {
             >
               <p>
                 Hi, I&apos;m <mark>Tushar</mark> a <mark>passionate</mark>{" "}
-                <br />
                 software developer.
               </p>
             </h1>
