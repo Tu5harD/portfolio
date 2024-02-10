47da9c93dc00b873c5c4fcc0feda18af1609759f
DevTusharDukane <tushardukane9@gmail.com>
2024-02-10 21:31:51 +0530

    Final Deployment

--- src/container/Header/index.js
+++ src/container/Header/index.js
@@ -46,6 +46,7 @@ const Header = () => {
       <section className=" -mt-14" ref={introRef}>
         <div className="grid grid-cols-1 md:grid-cols-[1fr_0.5fr] lg:grid-cols-[1fr_0.7fr] gap-4 items-center">
           <div className="py-5 md:py-10">
+            
             <h1
               tabIndex="0"
               ref={ref}
