e17f8e58e69bafcb50a0d0f2f31a40af708346e2
DevTusharDukane <tushardukane9@gmail.com>
2024-03-20 18:53:23 +0530

    icon bg change

--- src/components/Floating.js
+++ src/components/Floating.js
@@ -5,18 +5,21 @@ import { github, linkedin, whatsapp } from "../../public/assets";
 
 const Floating = () => {
   return (
-    <div className="fixed bottom-[20%] left-0 z-50 text-black sm:hidden bg-orange-100 px-2 py-4 rounded-lg shadow-lg -ml-1 shadow-blue-200">
-      <div className=" flex flex-col gap-2 items-center justify-center">
-        <Link href={"https://github.com/Tu5harD"}>
+    <div className="fixed bottom-[20%] left-0 z-50 text-black sm:hidden bg-orange-100 rounded-lg shadow-lg -ml-1 shadow-blue-200">
+      <div className=" flex flex-col  items-center justify-center">
+        <Link href={"https://github.com/Tu5harD"} className=" bg-[#ffffff]">
           <Image
             src={github}
-            className="ml-1 w-8 h-8 hover:origin-center hover:rotate-45"
+            className="ml-1 w-8 h-8 hover:origin-center hover:rotate-45 z-50 "
             alt=""
             width={40}
             height={40}
           />
         </Link>
-        <Link href={"https://www.linkedin.com/in/tushardukane/"}>
+        <Link
+          href={"https://www.linkedin.com/in/tushardukane/"}
+          className=" bg-[#0077B5]"
+        >
           <Image
             src={linkedin}
             className="ml-1 w-8 h-8 hover:origin-center hover:rotate-45"
@@ -25,7 +28,7 @@ const Floating = () => {
             height={40}
           />
         </Link>
-        <Link href={"https://wa.me/+917499833141"}>
+        <Link href={"https://wa.me/+917499833141"} className=" bg-[#25D366]">
           <Image
             src={whatsapp}
             alt=""
